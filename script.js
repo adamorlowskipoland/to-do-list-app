a2076b117a5fe9e906cec3b1eba1c251c3f1fbe9
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-07 17:48:02 +0100

    added a toggleAll function

--- script.js
+++ script.js
@@ -41,5 +41,31 @@ var todoList = {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
+    },
+    
+    
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        
+        //getting number of completed todos
+        for (var i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        //Case 1: if everything is true, make everything false
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        }
+        // Case 2: make everything true
+        else {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
     }
 };
