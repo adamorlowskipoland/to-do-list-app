60301b402e8f8e764a1dafb7dda3bc0449653648
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-07 02:46:37 +0100

    started with a small functions

---
+++ script.js
@@ -0,0 +1,30 @@
+// our todos
+var todos = ['item1', 'item2', 'item3'];
+
+
+// It should have a function to display todos.
+function displayTodos() {
+    console.log('My todos:', todos);
+}
+
+
+// It should have a function to add todos.
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodos();
+}
+
+
+
+// It should have a function to change todos.
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+    displayTodos();
+}
+
+
+// It should have a function to delete todos.
+function deleteTodo (position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
