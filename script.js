fa9827fab5f8ac7afeac0fa4a389cd57556fa264
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-07 03:30:46 +0100

    working on a object functions

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// our todos
-var todos = ['item1', 'item2', 'item3'];
-
-
-// It should have a function to display todos.
-function displayTodos() {
-    console.log('My todos:', todos);
-}
-
-
-// It should have a function to add todos.
-function addTodo(todo) {
-    todos.push(todo);
-    displayTodos();
-}
-
-
-
-// It should have a function to change todos.
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-
-// It should have a function to delete todos.
-function deleteTodo (position) {
-    todos.splice(position, 1);
-    displayTodos();
-}
-
-// building objects
-
-var myComputer = {
-    operatingSystem: 'mac',
-    screenSize: '13 in',
-    purchaseYear: 2012
+var todoList = {
+  todos: ['item1', 'item2', 'item3'],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  }
 };
