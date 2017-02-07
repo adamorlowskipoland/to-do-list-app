b80a3e1d580a792ab73d18043d50940641d6280e
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-07 22:25:39 +0100

    testing debugger on functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var todoList = {
     } else {
         console.log("My Todos:");
         for (var i = 0; i < this.todos.length; i++) {
-            if (this.todos.completed === true) {
+            if (this.todos[i].completed === true) {
                 console.log("(x)", this.todos[i].todoText);
             } else {
                 console.log("( )", this.todos[i].todoText);
@@ -38,6 +38,7 @@ var todoList = {
     
     
     toggleCompleted: function(position) {
+      debugger;
         var todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
