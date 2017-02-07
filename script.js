7bc6c2f3ee68b56280f4a0081cbd0680505ef66a
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-02-07 15:57:26 +0100

    displayTodos now shows all todos

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos', this.todos);
+    console.log('My Todos:');
+    for (var i = 0; i < this.todos.length; i++) {
+        this.todos[i].todoText;
+    }
   },
+    
+    
   addTodo: function(todoText) {
     this.todos.push({
         todoText: todoText,
@@ -10,14 +15,20 @@ var todoList = {
     });
     this.displayTodos();
   },
+    
+    
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
+    
+    
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
+    
+    
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
